12a156cdd0a186c0832b2cc157a2aa8dff5dd6a8
ImanFatimaa <168275744+ImanFatimaa@users.noreply.github.com>
2024-06-16 18:01:52 +0500

    Create BestPathFinder.java

---
+++ BestPathFinder.java
@@ -0,0 +1,27 @@
+package oop_project;
+
+public class Bestpathlist {
+	  private  int length;
+	    private String Path_name;
+	    private  int cost;
+		public int getLength() {
+			return length;
+		}
+		public void setLength(int len) {
+			length = len;
+		}
+		public String getPath_name() {
+			return Path_name;
+		}
+		public void setPath_name(String name) {
+			Path_name = name;
+		}
+		public int getCost() {
+			return cost;
+		}
+		public void setCost(int cst) {
+			cost = cst;
+		}
+
+
+}
